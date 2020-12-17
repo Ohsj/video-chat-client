3cb10d866f071591d159e3a9bb01d21743fef703
ohsj <zasqw456@namver.com>
2020-12-17 18:54:37 +0900

    add logger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { createServer } = require('http');
 const path = require('path');
+const logger = require('./config/winston');
 
 /**
  *@since
@@ -14,5 +15,5 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '../views')));
 
 server.listen(port, () => {
-    console.log(`video-chat-client listening on http://localhost:${port}`);
+    logger.info(`video-chat-client listening on http://localhost:${port}`);
 });
